2c0c20ad69ac530ab65a2376caef6bf783c7d83c
rjehf <seba19983@o2.pl>
2020-02-11 14:45:03 +0100

    Authorization by google get information about user

--- src/main/java/pl/kazmierski/openid/TestApi.java
+++ src/main/java/pl/kazmierski/openid/TestApi.java
@@ -1,9 +1,17 @@
 package pl.kazmierski.openid;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.LinkedHashMap;
+
+@Controller
 public class TestApi {
 
     @GetMapping("/test1")
@@ -12,7 +20,18 @@ public class TestApi {
     }
 
     @GetMapping("/test2")
-    public String test2() {
-        return "test2";
+    public String test2(Model model) {
+
+        Object details = ((UsernamePasswordAuthenticationToken)((OAuth2Authentication)((SecurityContextImpl) SecurityContextHolder.getContext()).getAuthentication()).getUserAuthentication()).getDetails();
+       String name = ((LinkedHashMap)details).values().toArray()[1].toString();
+       String picture = ((LinkedHashMap)details).values().toArray()[4].toString();
+        String email = ((LinkedHashMap) details).values().toArray()[5].toString();
+
+        model.addAttribute("name", name);
+        model.addAttribute("picture", picture);
+        model.addAttribute("email", email);
+
+
+        return "gui";
     }
 }
